14b69b944a6c3bd3fa5c9de89fea5dbd7b7c4e65
KellyRudnicki <kelly.rudnicki@comcast.net>
2020-03-23 18:11:52 -0600

    Initial Commit

---
+++ woksape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  woksape
+//
+//  Created by Kelly Rudnicki on 3/23/20.
+//  Copyright © 2020 Kelly Rudnicki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
